68e2bb2a6125adb0f8860876e310b0702e4e2678
scythe410 <107651261+scythe410@users.noreply.github.com>
2024-12-04 15:11:03 +0530

    added getALL

--- votermanager.py
+++ votermanager.py
@@ -1,5 +1,5 @@
 import sqlite3 as db
-
+from entity.voter import Voter
 
 def initializeDatabase():
     sql = "CREATE TABLE IF NOT EXISTS voter(iitNumber int(8) PRIMARY KEY, fName varchar(10), lName varchar(10))"
@@ -30,3 +30,21 @@ def updateVoter(iitNumber, fName, lName):
     oUser.fName = fName
     oUser.lName = lName
     oUser.update()
+
+def getAllVoters():
+    oVoters = []
+    sql = "SELECT iitNumber, fName, lName FROM voter"
+    con = db.connect("database\\voter.db")
+    cur = con.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    con.commit()
+    cur.close()
+    con.close()
+    for row in rows:
+        oVoter = Voter()
+        oVoter.iitNumber = row[0]
+        oVoter.fName = row[1]
+        oVoter.lName = row[2]
+        oVoters.append(Voter)
+    return Voters
