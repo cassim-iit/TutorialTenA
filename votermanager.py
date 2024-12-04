e9c4014b72aceb45506389e0c436f9d4e4b5e5cb
scythe410 <107651261+scythe410@users.noreply.github.com>
2024-12-04 14:10:39 +0530

    created voter db

---
+++ votermanager.py
@@ -0,0 +1,12 @@
+import sqlite3 as db
+
+
+def initializeDatabase():
+    sql = "CREATE TABLE IF NOT EXISTS voter(iitNumber int(8) PRIMARY KEY, fName varchar(10), lName varchar(10);"
+    con = db.connect("database\\voter.db")
+    cur = con.cursor()
+    cur.execute(sql)
+    con.commit()
+    cur.close()
+    con.close()
+
