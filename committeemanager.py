f92545ef5fcd10d4495fe3a0cd170c2b02872c58
Hariksan <Hariksank.23@gmail.com>
2024-12-04 14:55:32 +0530

    Harish

--- committeemanager.py
+++ committeemanager.py
@@ -1,7 +1,7 @@
 import sqlite3 as db
 
 def initializeDatabase():
-    sql = "CREATE TABLE IF NOT EXISTS committee( Com_id TEXT PRIMARY KEY, name TEXT, age int, address TEXT )"
+    sql = "CREATE TABLE IF NOT EXISTS committee( Com_id INT AUTO_INCREMENT PRIMARY, name TEXT, age int, address TEXT )"
     con = db.connect("database\\committee.db")
     cur = con.cursor()
     cur.execute(sql)
@@ -9,5 +9,12 @@ def initializeDatabase():
     cur.close()
     con.close()
 
-initializeDatabase()
+def add(name, age, address):
+    sql = "INSERT INTO committee VALUES (name, age , address);"
+    con= db.connect("database\\committee.db")
+    cur = con.cursor()
+    cur.execute(sql)
+    con.commit()
+    cur.close()
+    con.close()
 
